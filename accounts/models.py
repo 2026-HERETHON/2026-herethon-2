334fdc361a22efb15385157e410492ab28679f4e
0hyunj <2476008@ewha.ac.kr>
2026-07-08 18:44:44 +0900

    feat: accounts, core,worker models.py 작성

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,9 @@ class User(AbstractUser):
     onboarding_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return self.email
