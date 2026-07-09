b623d2ca9dde0a9c1e9cc8f51c297a6a6d762056
0hyunj <2476008@ewha.ac.kr>
2026-07-09 11:43:36 +0900

    feat: 구글 로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('auth/google/login/', views.google_login, name='google_login'),
+    path('auth/google/callback/', views.google_callback, name='google_callback'),
+    path('logout/', views.logout_view, name='logout'),
 ]
