4e17003f5c6c1b459f1eb87cc7a0f11f8fb491c5
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-07-11 01:47:17 +0900

    feat: 마이페이지 모달 설정

--- static/js/worker_mypage.js
+++ static/js/worker_mypage.js
@@ -18,3 +18,47 @@ tabBtns.forEach((btn) => {
     targetContent.classList.add("active");
   });
 });
+
+// 근무 조건 수정 모달 설정
+function openModal(modalEl) {
+  modalEl.style.display = "flex";
+}
+
+function closeModal(modalEl) {
+  modalEl.style.display = "none";
+}
+
+// 희망 직무 모달
+const jobModal = document.getElementById("job-modal");
+const openJobBtn = document.getElementById("open-job-modal");
+const closeJobBtn = document.getElementById("done-job-modal");
+
+openJobBtn.addEventListener("click", () => openModal(jobModal));
+closeJobBtn.addEventListener("click", () => closeModal(jobModal));
+
+// 보유 스킬 모달
+const skillModal = document.getElementById("skill-modal");
+const openSkillBtn = document.getElementById("open-skill-modal");
+const closeSkillBtn = document.getElementById("done-skill-modal");
+
+openSkillBtn.addEventListener("click", () => openModal(skillModal));
+closeSkillBtn.addEventListener("click", () => closeModal(skillModal));
+
+// 가용 시간 모달
+const scheduleModal = document.getElementById("schedule-modal");
+const openScheduleBtn = document.getElementById("open-schedule-modal");
+const closeScheduleBtn = document.getElementById("done-schedule-modal");
+
+openScheduleBtn.addEventListener("click", () => openModal(scheduleModal));
+closeScheduleBtn.addEventListener("click", () => closeModal(scheduleModal));
+
+// 모달 배경 클릭 처리
+const allModals = [jobModal, skillModal, scheduleModal];
+
+allModals.forEach((modal) => {
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal(modal);
+    }
+  });
+});
